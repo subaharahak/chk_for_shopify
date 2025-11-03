c88e0b0258b6205fc3be73d7ee87b8dfb051fe72
subaharahak <subaharahak@gmail.com>
2025-11-03 23:42:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ function find_between($content, $start, $end)
 }
 
 $proxy_list_str = '
-51.159.85.23:6060:dljy1unah6650fl:fzs0dmxxapflhiy';
+51.159.85.23:6060:dljy1unah6650fl:acgseconac1wm39';
 
 $proxies = explode("\n", $proxy_list_str);
 $proxy_array = [];
